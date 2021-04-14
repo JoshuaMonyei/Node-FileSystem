6622534a860e5923c590e6180d7d0a7f7528d18e
Joshua Monyei <monyeijosh01@gmail.com>
2021-04-14 10:05:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const fetch = require('node-fetch');
 const fs = require("fs");
 
-const fetchData = () => {
-    const url = 'https://jsonplaceholder.typicode.com/todos/1'
-    fetch(url).then((res) => res.json()).then(data =>  {console.log('Data preview:','\n', data);
-        let info = JSON.stringify(data, null, 2);
-        fs.writeFile('./result/posts.json', info, (err) => {
-        if (err) throw err;
-        console.log('file created successfully')})
-    }).catch(err => {console.log(err)})
-
+const fetchData = async() => {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    const response = await fetch(url);
+    const data = await response.json();
+    let info = JSON.stringify(data, null, 2);
+    return info; 
 }
-fetchData()
+
+fetchData().then(info => {
+    fs.writeFile('./result/posts.json', info, (err) => {
+        if (err) throw err;
+        console.log('file created successfully');
+    });
+}).catch(err => {console.log(err)});
 
